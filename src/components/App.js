a7467c0c381584487ff9e285935f265fa9924f34
Admin <gherbi.yamin.y@gmail.com>
2020-03-25 19:54:43 +0100

    add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,17 @@ import HomePage from './HomePage'
 import AboutPage from './AboutPage'
 import Header from './common/Header'
 import CoursesPage from './CoursesPage'
+import {Route} from 'react-router-dom'
+
 
 function App() {
-    function getPage() {
-        const route = window.location.pathname;
-        if (route === "/courses") return <CoursesPage />
-        if (route === "/about") return <AboutPage/>
-        return <HomePage/>
-    }
-
-    return (<div className="container-fluid">
+    return (
+        <div className="container-fluid">
             <Header/>
-            {getPage()} </div>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage}/>
+            <Route path="/courses" component={CoursesPage}/>
+        </div>
     )
 }
 
